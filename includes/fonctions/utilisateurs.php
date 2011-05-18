d5c09e31ceb24ce611b1c59ae93579ee7910332f
Tomss <tom.cellier@gmail.com>
2011-05-18 23:16:20 +0200

    fonction getInfosUser
    Change-Id: I507967dd53eea0c3f5246a098b53bd21c08b55de

---
+++ includes/fonctions/utilisateurs.php
@@ -0,0 +1,58 @@
+<?php
+require_once '../config/main_config.php';
+
+/*
+function addUser($utilisateurLogin, $utilisateurMdp, $utilisateurType, $utilisateurJeton, $utilisateurInscriptionDate, $utilisateurNom, $utilisateurPrenom, $utilisateurCivilite, $utilisateurVille, $utilisateurCp, $utilisateurAdresse, $utilisateurEtat){
+	try {
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$bdd = new PDO(CONNECTION_STRING, LOGIN, MDP, $pdo_options);
+		$bdd->exec("SET CHARACTER SET utf8");
+		
+		$req = $bdd->prepare('INSERT INTO formations(formation_titre) VALUES(:formation_titre)');
+		$req->execute(array(
+		'formation_titre' => $formationTitre
+		));
+				
+	}
+	catch(Exception $e)
+	{
+		if(!PROD)
+		{
+			echo $e->getMessage();
+		}
+	}
+}
+*/
+function getInfosUser($utilisateurLogin){
+	try 
+	{
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$bdd = new PDO(CONNECTION_STRING, LOGIN, MDP, $pdo_options);
+		$bdd->exec("SET CHARACTER SET utf8");
+		
+		$req = $bdd->prepare('SELECT utilisateur_mdp, utilisateur_type FROM sd_utilisateurs WHERE utilisateur_login = ?');
+		$req->execute(array($utilisateurLogin));
+		if($req->rowCount() != 0)
+		{
+			$donnees = $req->fetch();
+			return $donnees;
+		}
+		else 
+		{
+			return false;
+		}
+				
+		$req->closeCursor();
+	}
+	catch(Exception $e)
+	{
+		if(!PROD)
+		{
+			echo $e->getMessage();
+		}
+	}
+	
+	
+}
+
+?>
