9326ee431c28271a8fc378819a7b09509cf041bc
Tomss <tom.cellier@gmail.com>
2011-05-20 02:21:03 +0200

    Change-Id: Idef2a0fc530dac4830021b20e9d57a18a71329b2

--- includes/fonctions/formations.php
+++ includes/fonctions/formations.php
@@ -1,26 +1,83 @@
 <?php
-/**
- * 
- * Ajouter une formation
- * @param char $formationTitre titre de la formation
+
+/*
+ * Ajout d'une formation
  */
-function addFormation($formationTitre){
+function addFormation($formationPlace, $formationType, $formationVille, $formationDate){
 	try {
 		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 		$bdd = new PDO(CONNECTION_STRING, LOGIN, MDP, $pdo_options);
 		$bdd->exec("SET CHARACTER SET utf8");
+
 		
-		$req = $bdd->prepare('INSERT INTO formations(formation_titre) VALUES(:formation_titre)');
+		$req = $bdd->prepare('INSERT INTO sd_formations(formation_place, formation_type, formation_ville, formation_date) VALUES(:formation_place, :formation_type, :formation_ville, :formation_date)');
 		$req->execute(array(
-		'formation_titre' => $formationTitre
+		'formation_place' => $formationPlace,
+		'formation_type' => $formationType,
+		'formation_ville' => $formationVille,
+		'formation_date' => $formationDate
 		));
 				
+		$req->closeCursor();
 	}
 	catch(Exception $e)
 	{
-		if(!PROD){
+		if(!PROD)
+		{
 			echo $e->getMessage();
 		}
 	}
 }
+
+
+/*
+ * Récuperer les infos d'une formation
+ * Pas fini
+ */
+function getInfosFormation($filtre, $type, $ville){
+	try 
+	{
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$bdd = new PDO(CONNECTION_STRING, LOGIN, MDP, $pdo_options);
+		$bdd->exec("SET CHARACTER SET utf8");
+		
+		//Récupération des formations 
+		switch ($filtre){
+			case 'place' :
+				$req = $bdd->query('SELECT formation_place, formation_type, formation_ville, formation_date FROM sd_formations WHERE formation_place < 0 AND NOW() < formation_date < DATE_ADD(NOW(), INTERVAL 10 DAYS)');
+				if($req->rowCount() != 0)
+				{
+					$donnees = $req->fetch();
+					return $donnees;
+				}
+				else 
+				{
+					return false;
+				}
+				
+				$req->closeCursor();
+			break;
+				
+			case 'type' :
+				
+				break;
+				
+			case 'ville':
+				break;
+		}
+		
+				
+		$req->closeCursor();
+	}
+	catch(Exception $e)
+	{
+		if(!PROD)
+		{
+			echo $e->getMessage();
+		}
+	}
+}
+
+
+
 ?>
